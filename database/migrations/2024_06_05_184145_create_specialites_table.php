57c4f1ee061dec7b6ba79b5f3392dc11cf5a1243
Doriel <mapangounzigoudaniellevy@gmail.com>
2024-06-05 19:17:26 +0000

    Database logic

---
+++ database/migrations/2024_06_05_184145_create_specialites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Medecin;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('specialites', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('status');
+            $table->foreignIdFor(Medecin::class)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('specialites');
+    }
+};
